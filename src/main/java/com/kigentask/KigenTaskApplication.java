9130379ad4a8d1f9f5670a21d15d25f09a80b36d
Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
2026-04-01 13:04:49 -0600

    feat(core): bootstrap app, base config and health endpoint

---
+++ src/main/java/com/kigentask/KigenTaskApplication.java
@@ -0,0 +1,12 @@
+package com.kigentask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KigenTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KigenTaskApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kigentask/KigenTaskApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kigentask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KigenTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
